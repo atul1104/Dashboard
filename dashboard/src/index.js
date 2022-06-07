edf825b76735847a7bfcbf05c434cad9d403ddf1
atul1104 <waghamareatul2@gmail.com>
2022-06-08 01:50:24 +0530

    dashboard app

---
+++ dashboard/src/index.js
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import ReactDOM from "react-dom";
+
+import "./Style/index.css";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+
+import App from "./Components/App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>,
+  rootElement
+);
